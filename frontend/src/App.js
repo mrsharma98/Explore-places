75921ab888d8a6935429dc423d89598fce8868b9
Manish Sharma <mrsharma98@gmail.com>
2022-12-31 19:28:27 +0530

    added form for new and update places

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Users from './user/pages/Users';
 import NewPalce from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path="/places/new" exact>
             <NewPalce />
+          </Route>
+          <Route path="/places/:placeId" exact>
+            <UpdatePlace />
           </Route>
 
           <Redirect to="/" />
--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,6 +1,70 @@
-function NewPalce() {
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button'
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/Validators.js';
+
+import { useForm } from '../../shared/hooks/form-hook';
+
+import './PlaceForm.css'
+
+
+
+const NewPalce = () => {
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      },
+      address: {
+        value: '',
+        isValid: false
+      }
+    },
+    false
+  )
+
+
+
+  const placeSubmitHandler = event => {
+    event.preventDefault()
+    console.log(formState.inputs)
+  }
+
+
   return (
-    <h2>NewPlace Works!</h2>
+    <form className='place-form' onSubmit={placeSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description"
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address"
+        onInput={inputHandler}
+      />
+      <Button type='submit' disabled={!formState.isValid}>ADD PLACE</Button>
+    </form>
   );
 }
 
